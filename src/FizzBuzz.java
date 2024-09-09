93afff6ce09f3484bad693bd476eba6081af8571
Chiraz Belarbi <c.b.@lalalalala.local>
2024-09-09 14:45:29 -0400

    refactored to extract helper method and modified README.md

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -1,10 +1,35 @@
-/**
+/*
  * Solve the FizzBuzz challenge.
- */
+*/
+
 class FizzBuzz {
 
     public static void main(String[] args) {
 
+        int i = 1;
+        while (i <= 100) {
+            i = doFizzBuzz(i);
+        }
+    }
+
+    private static int doFizzBuzz(int i) {
+        boolean DivideBy3 = i % 3 == 0;
+        boolean DivideBy5 = i % 5 == 0;
+        if (DivideBy3 && DivideBy5) {
+            System.out.println("FizzBuzz");
+        } else if (DivideBy3) {
+            System.out.println("Fizz");
+        } else if (DivideBy5) {
+            System.out.println("Buzz");
+        } else {
+            System.out.println(i);
+        }
+        i++;
+        return i;
+    }
+
+
+    private static void extracted() {
         for (int i = 1; i < 100; i++) {
 
             // Find out which numbers divide i.
